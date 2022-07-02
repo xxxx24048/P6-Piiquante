16113dd90f5278914b033a734f48ae281c18b177
xxxx24048 <102151407+xxxx24048@users.noreply.github.com>
2022-07-02 16:47:23 +0200

    init

---
+++ backend/app.js
@@ -0,0 +1,32 @@
+const express = require('express');
+
+const app = express();
+const mongoose = require('mongoose');
+
+mongoose.connect('mongodb+srv://new_user:Eqpe0Lk7YzL0HI0I@cluster0.gnrcq.mongodb.net/?retryWrites=true&w=majority',
+  { useNewUrlParser: true,
+    useUnifiedTopology: true })
+  .then(() => console.log('Connexion à MongoDB réussie !'))
+  .catch(() => console.log('Connexion à MongoDB échouée !'));
+
+
+app.use((req, res, next) => {
+    console.log('Requête reçue !');
+    next();
+  });
+  
+  app.use((req, res, next) => {
+    res.status(201);
+    next();
+  });
+  
+  app.use((req, res, next) => {
+    res.json({ message: 'Votre requête a bien été reçue !' });
+    next();
+  });
+  
+  app.use((req, res, next) => {
+    console.log('Réponse envoyée avec succès !');
+  });
+ module.exports = app;
+
